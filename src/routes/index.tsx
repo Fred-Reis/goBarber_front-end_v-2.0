80a2deabaad39309ac2c95ad21e65258f82ef6e4
FredReis <52092659+Fred-Reis@users.noreply.github.com>
2020-06-27 18:03:31 -0300

    Update index.tsx
    adjust imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Route from './Route';
-
-import Dashboard from '../pages/Dashboard';
 
 const Routes = () => {
   return (
